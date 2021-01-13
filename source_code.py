80bf8e5ac112250a2969b5cfc20d1407c9e6dd60
Tridiv Kumar Rabha <tridivkumar@gmail.com>
2021-01-13 13:43:52 +0530

    debugged and stored compressed file

--- source_code.py
+++ source_code.py
@@ -1,141 +1,140 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
-import matplotlib.image as img 
-# from scipy.io import loadmat 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+# from scipy.io import loadmat
 from matplotlib.pyplot import imread
 
 from PIL import Image
 
 import urllib
-import cv2
 import numpy as np
 import os
 
 
-def read_image(): 
-    img = Image.open("krangshuri.jpg")
+def read_image():
+    img = Image.open("bird_small.png")
     print(img)
     # img = img/255
     img = np.asarray(img) / 255
 
     return img
 
-def initialize_means(img, clusters): 
-    
-    # reshaping it or flattening it into a 2d matrix 
-    points = np.reshape(img, (img.shape[0] * img.shape[1], 
-                                            img.shape[2])) 
-    m, n = points.shape 
+def initialize_means(img, clusters):
 
-    # clusters is the number of clusters 
-    # or the number of colors that we choose. 
-    
-    # means is the array of assumed means or centroids. 
-    means = np.zeros((clusters, n)) 
+    # reshaping it or flattening it into a 2d matrix
+    points = np.reshape(img, (img.shape[0] * img.shape[1],
+                                            img.shape[2]))
+    m, n = points.shape
 
-    # random initialization of means. 
-    for i in range(clusters): 
-        rand1 = int(np.random.random(1)*10) 
-        rand2 = int(np.random.random(1)*8) 
-        means[i, 0] = points[rand1, 0] 
-        means[i, 1] = points[rand2, 1] 
+    # clusters is the number of clusters
+    # or the number of colors that we choose.
 
-    return points, means 
+    # means is the array of assumed means or centroids.
+    means = np.zeros((clusters, n))
 
+    # random initialization of means.
+    for i in range(clusters):
+        rand1 = int(np.random.random(1)*10)
+        rand2 = int(np.random.random(1)*8)
+        means[i, 0] = points[rand1, 0]
+        means[i, 1] = points[rand2, 1]
 
-# Function to measure the euclidean 
-# distance (distance formula) 
-def distance(x1, y1, x2, y2): 
-    
-    dist = np.square(x1 - x2) + np.square(y1 - y2) 
-    dist = np.sqrt(dist) 
-
-    return dist 
+    return points, means
 
 
-def k_means(points, means, clusters): 
+# Function to measure the euclidean
+# distance (distance formula)
+def distance(x1, y1, x2, y2):
 
-    iterations = 10 # the number of iterations 
-    m, n = points.shape 
-    
-    # these are the index values that 
-    # correspond to the cluster to 
-    # which each pixel belongs to. 
-    index = np.zeros(m) 
+    dist = np.square(x1 - x2) + np.square(y1 - y2)
+    dist = np.sqrt(dist)
 
-    # k-means algorithm. 
-    while(iterations > 0): 
+    return dist
 
-        for j in range(len(points)): 
-            
-            # initialize minimum value to a large value 
+
+def k_means(points, means, clusters):
+
+    iterations = 10 # the number of iterations
+    m, n = points.shape
+
+    # these are the index values that
+    # correspond to the cluster to
+    # which each pixel belongs to.
+    index = np.zeros(m)
+
+    # k-means algorithm.
+    while(iterations > 0):
+
+        for j in range(len(points)):
+
+            # initialize minimum value to a large value
             minv = 1000
             temp = None
-            
-            for k in range(clusters): 
-                
-                x1 = points[j, 0] 
-                y1 = points[j, 1] 
-                x2 = means[k, 0] 
-                y2 = means[k, 1] 
-                
-                if(distance(x1, y1, x2, y2) < minv):         
-                    minv = distance(x1, y1, x2, y2) 
-                    temp = k 
-                    index[j] = k 
-        
-        for k in range(clusters): 
-            
+
+            for k in range(clusters):
+
+                x1 = points[j, 0]
+                y1 = points[j, 1]
+                x2 = means[k, 0]
+                y2 = means[k, 1]
+
+                if(distance(x1, y1, x2, y2) < minv):
+                    minv = distance(x1, y1, x2, y2)
+                    temp = k
+                    index[j] = k
+
+        for k in range(clusters):
+
             sumx = 0
             sumy = 0
             count = 0
-            
-            for j in range(len(points)): 
-                
-                if(index[j] == k): 
-                    sumx += points[j, 0] 
-                    sumy += points[j, 1] 
+
+            for j in range(len(points)):
+
+                if(index[j] == k):
+                    sumx += points[j, 0]
+                    sumy += points[j, 1]
                     count += 1
-            
-            if(count == 0): 
-                count = 1   
-            
-            means[k, 0] = float(sumx / count) 
-            means[k, 1] = float(sumy / count)    
-            
+
+            if(count == 0):
+                count = 1
+
+            means[k, 0] = float(sumx / count)
+            means[k, 1] = float(sumy / count)
+
         iterations -= 1
 
-    return means, index 
+    return means, index
 
 
-def compress_image(means, index, img): 
+def compress_image(means, index, img):
 
-    # recovering the compressed image by 
-    # assigning each pixel to its corresponding centroid. 
-    centroid = np.array(means) 
-    recovered = centroid[index.astype(int), :] 
-    
-    # getting back the 3d matrix (row, col, rgb(3)) 
-    recovered = np.reshape(recovered, (img.shape[0], img.shape[1], 
-                                                    img.shape[2])) 
+    # recovering the compressed image by
+    # assigning each pixel to its corresponding centroid.
+    centroid = np.array(means)
+    recovered = centroid[index.astype(int), :]
 
-    # plotting the compressed image. 
-    plt.imshow(recovered) 
-    plt.show() 
+    # getting back the 3d matrix (row, col, rgb(3))
+    recovered = np.reshape(recovered, (img.shape[0], img.shape[1],
+                                                    img.shape[2]))
 
-    # saving the compressed image. 
-    misc.imsave('compressed_' + str(clusters) +
-                        '_colors.png', recovered) 
+    # plotting the compressed image.
+    plt.imshow(recovered)
+    plt.show()
+
+    # saving the compressed image.
+    plt.imsave('compressed_' + str(clusters) +
+                        '_colors.png', recovered)
 
 
-# Driver Code 
-if __name__ == '__main__': 
+# Driver Code
+if __name__ == '__main__':
 
-    img = read_image() 
+    img = read_image()
 
     clusters = 16
-    clusters = int(input('Enter the number of colors in the compressed image. default = 16\n')) 
+    clusters = int(input('Enter the number of colors in the compressed image\n'))
 
-    points, means = initialize_means(img, clusters) 
-    means, index = k_means(points, means, clusters) 
-    compress_image(means, index, img) 
+    points, means = initialize_means(img, clusters)
+    means, index = k_means(points, means, clusters)
+    compress_image(means, index, img)
